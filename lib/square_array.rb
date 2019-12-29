1686af5be4abd3f6a7382fa59905e9feb439ee83
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-29 05:37:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,11 @@
 def square_array(array)
   counter = 0
-  newarr = []
 
   while counter < array.length do
-    newarr.push(array[counter] ** 2)
+    array[counter] = array[counter] ** 2  
     counter += 1
   end
 
-return newarr
+  return array
 
 end
